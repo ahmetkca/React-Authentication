85d8bdd39688ab2b647ea6c31765731d90ebef0d
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-08-22 17:55:22 -0400

    Use Buffer instead of atob() since it is deprecated

--- front-end/src/auth/useUser.js
+++ front-end/src/auth/useUser.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useJwtToken } from "./useJwtToken";
+import { Buffer } from "buffer";
 
 export const useUser = () => {
     const [jwtToken] = useJwtToken();
@@ -7,7 +8,7 @@ export const useUser = () => {
     const getPayloadFromJwtToken = (token) => {
         console.log(token);
         const payload = token.split(".")[1];
-        return JSON.parse(atob(payload));
+        return JSON.parse(Buffer.from(payload, "base64").toString("ascii"));
     }
 
     const [user, setUser] = useState(() => {
