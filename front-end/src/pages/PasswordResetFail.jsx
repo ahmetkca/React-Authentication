5a61072352bcadaa8201d2ce93d77ad1419daa16
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-08-23 15:47:45 -0400

    Added Reset password landing page

---
+++ front-end/src/pages/PasswordResetFail.jsx
@@ -0,0 +1,10 @@
+export const PasswordResetFail = () => {
+    return (
+        <div>
+            <h1>Password Reset Failed</h1>
+            <p>Your password reset failed. Please try again.</p>
+        </div>
+    )
+}
+
+export default PasswordResetFail;
